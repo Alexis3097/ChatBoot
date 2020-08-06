59fb51376424c06a6bf8bbcfa978cda41f448afe
Felipe Martínez <40834220+luisfelipemo@users.noreply.github.com>
2020-08-05 22:17:14 -0500

    Que onda
    Hola bb

--- bot/chat.php
+++ bot/chat.php
@@ -4,7 +4,10 @@ $bot = new Bot;
 $questions = [
     //que somos
     "¿que es?" => "centro especializado en Acupuntura-naturopatia y geriatría. diplomados profesionales en salud alternativa",
+    "informacion" => "centro especializado en Acupuntura-naturopatia y geriatría. diplomados profesionales en salud alternativa",
+    "información" => "centro especializado en Acupuntura-naturopatia y geriatría. diplomados profesionales en salud alternativa",
     "que es?" => "centro especializado en Acupuntura-naturopatia y geriatría. diplomados profesionales en salud alternativa",
+    "que és?" => "centro especializado en Acupuntura-naturopatia y geriatría. diplomados profesionales en salud alternativa",
     "que son?" =>"centro especializado en Acupuntura-naturopatia y geriatría. diplomados profesionales en salud alternativa",
     "¿que son?" =>"centro especializado en Acupuntura-naturopatia y geriatría. diplomados profesionales en salud alternativa",
     "¿a que se dedican?" =>"centro especializado en Acupuntura-naturopatia y geriatría. diplomados profesionales en salud alternativa",
@@ -14,6 +17,14 @@ $questions = [
     //otros temas
     "como estas?" =>"muy bien, pasando un excelente dia, espero que el tuyo tambien.",
     "¿como estas?" =>"muy bien, pasando un excelente dia, espero que el tuyo tambien.",
+    "¿Te pueded dar covid?" =>"jaja, a los bots no nos da coronavirus :).",
+    "Te pueded dar covid?" =>"jaja, a los bots no nos da coronavirus :).",
+    "¿Tienes covid?" =>"jaja, a los bots no nos da coronavirus :).",
+    "Tienes covid?" =>"jaja, a los bots no nos da coronavirus :).",
+    "¿Te pueded dar coronavirus?" =>"jaja, a los bots no nos da coronavirus :).",
+    "Te pueded dar coronavirus?" =>"jaja, a los bots no nos da coronavirus :).",
+    "¿Tienes coronavirus?" =>"jaja, a los bots no nos da coronavirus :).",
+    "Tienes coronavirus?" =>"jaja, a los bots no nos da coronavirus :).",
     //redes sociales
     "como contactar?" => "Puedes marcar el numero961 173 4245 tambien mandarnos mensaje privado en nuestro facebook https://www.facebook.com/acumedictux o un correo a medicalternativa@hotmail.com",
     "contacto?" => "Puedes marcar el numero 961 173 4245 tambien mandarnos mensaje privado en nuestro facebook https://www.facebook.com/acumedictux o un correo a medicalternativa@hotmail.com",
@@ -22,6 +33,9 @@ $questions = [
     "como encontrarlos?" => "Puedes marcar el numero 961 173 4245 tambien mandarnos mensaje privado en nuestro facebook https://www.facebook.com/acumedictux o un correo a medicalternativa@hotmail.com",
     "contacto" => "Puedes marcar el numero 961 173 4245 tambien mandarnos mensaje privado en nuestro facebook https://www.facebook.com/acumedictux o un correo a medicalternativa@hotmail.com",
     //Horarios de atencion
+    "¿A qué hora puedo llegar?"=>"De lunes a viernes de 10:00 - 20:00 horas",
+    "A qué hora puedo llegar?"=>"De lunes a viernes de 10:00 - 20:00 horas",
+    "A qué hora puedo llegar"=>"De lunes a viernes de 10:00 - 20:00 horas",
     "horario de atencion"=>"De lunes a viernes de 10:00 - 20:00 horas",
     "horario"=>"De lunes a viernes de 10:00 - 20:00 horas",
     "horarios"=>"De lunes a viernes de 10:00 - 20:00 horas",
@@ -36,7 +50,9 @@ $questions = [
     "que horas abren"=>"De lunes a viernes de 10:00 - 20:00 horas",
     "Todo el dia esta abierto?"=>"De lunes a viernes de 10:00 - 20:00 horas",
     "¿Todo el dia esta abierto?"=>"De lunes a viernes de 10:00 - 20:00 horas",
-
+    //Precios
+    "¿Qué precios manejan?"=>"De lunes a viernes de 10:00 - 20:00 horas",
+    "¿Qué precios manejan?"=>"De lunes a viernes de 10:00 - 20:00 horas",
     //donde encontrarnos
     "donde estan?"=>"Nos ubicamos en monseni 124 fraccionamiento montserrat 29070 Tuxtla Gutiérrez, Chiapas, México",
     "donde estan"=>"Nos ubicamos en monseni 124 fraccionamiento montserrat 29070 Tuxtla Gutiérrez, Chiapas, México",
@@ -56,6 +72,9 @@ $questions = [
     "¿ubicacion?"=>"Nos ubicamos en monseni 124 fraccionamiento montserrat 29070 Tuxtla Gutiérrez, Chiapas, México",
     
     //name
+    "quien eres" =>"Soy AcumedicBoot y estoy para servirte",
+    "¿quien eres?" =>"Soy AcumedicBoot y estoy para servirte",
+    "quien eres?" =>"Soy AcumedicBoot y estoy para servirte",
     "como te llamas?" =>"Soy AcumedicBoot y estoy para servirte",
     "cual es tu nombre?" =>"Soy AcumedicBoot y estoy para servirte",
     "tienes nombre?" =>"Soy AcumedicBoot y estoy para servirte",
